f3c123252b8d866477e11a32589f771053e9fff8
kassandra <kassandra_lira@hotmail.com>
2019-10-12 21:21:12 -0300

    contribuição 3

---
+++ aula_C06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int minhaIdade = 35;
+
+int paiIdade = 60;
+
+int maeIdade = 62;
+
+printf("Minha idade = %i\nMãe idade = %i\nPai idade = %i\n", minhaIdade, paiIdade, maeIdade);
+
+
+    return 0;
+}
