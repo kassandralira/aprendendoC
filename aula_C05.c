e9f7b7be011c8f0953668a23498754162c519a9f
kassandra <kassandra_lira@hotmail.com>
2019-10-12 21:18:17 -0300

    outro arquivo

---
+++ aula_C05.c
@@ -0,0 +1,23 @@
+/*
+gcc aula C05.c -o aulac05
+./aulac05
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+
+int minhaIdade;
+
+minhaIdade = 35;
+
+printf ("Minha idade é = %i.\n", minhaIdade);
+
+return 0;
+
+
+
+}
